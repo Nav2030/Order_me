8f28fa02eea9cc2bd7c27312fd33d23ca39a9aed
Rishi0721 <rishmikalavandi@gmail.com>
2019-05-30 08:37:23 +0530

    Terms and conditions

--- lib/Pages/terms.dart
+++ lib/Pages/terms.dart
@@ -13,15 +13,34 @@ class _TermsState extends State<Terms> {
         title: Text('Terms And Conditions'),
       ),
       body:  SingleChildScrollView(
-        child: new Column(
-      children: <Widget>[
-        new ListTile(
-          title: new Text('Menu is displayed as the availability of the menu items in the mapped restaurant.'),
-          subtitle: new Text('In case certain menu items are not listed in the menu page. In case of non availability of ordered product at the mapped restaurant, the order would not be executed. Same would be informed by the restaurant to you.'),
-        ),
-        new ListTile(
-          title: new Text('Our Website may contain our service marks or trademarks as well as those of our affiliates or other companies, in the form of words, graphics, and logos. Your use of our Website does not constitute any right or license for you to use such service marks/trademarks, without the prior written permission of the corresponding service mark/trademark owner. Our Website is also protected under international copyright laws. The copying, redistribution, use or publication by you of any portion of our Website is strictly prohibited. Your use of our Website does not grant you ownership rights of any kind in our Website.'),
-        )
+        child: new ListView(
+          children: <Widget>[
+            new ListTile(
+              title: new Text('Menu is displayed as the availability of the menu items in the mapped restaurant.',
+              new TextStyle()
+              ),
+            ),
+            new ListTile(
+              title: new Text('In case certain menu items are not listed in the menu page. In case of non availability of ordered product at the mapped restaurant, the order would not be executed. Same would be informed by the restaurant to you.'),
+            ),
+            new ListTile(
+              title: new Text('The online order once placed cannot be modified or cancelled either through the website or offline by calling the restaurant.'),
+            ),
+            new ListTile(
+              title: new Text('However you can get updated on the status of the order by calling the restaurant directly'),
+            ),
+            new ListTile(
+              title: new Text('In case the order which is paid through credit card is cancelled due to non availability of the ordered product at the restaurant, the amount will be returned by reversing the transaction by our restaurant. The same would reflect in the next credit card statement.'),
+            ),
+            new ListTile(
+              title: new Text('Order Me may collect personal information from you including your first and last name, address, telephone and mobile number(s), email address and any other information, when you knowingly provide us with this information.'),
+            ),
+            new ListTile(
+              title: new Text('Order Me may use the personal information that you submit to store and process that information in order to provide goods and services. Generally, we may use your personal information in the ways in which you would expect, including but not limited.'),
+            ),
+            new ListTile(
+              title: new Text('Order Me does not store any of your credit card details'),
+            ),
       ],
     ),
       ),
